6504b88a12e26e00751d6e644c053b9ea789ed7c
caedes <laurentromain@gmail.com>
2017-01-03 16:32:03 +0100

    feat: add interactive choices on CLI

--- src/bump-it.js
+++ src/bump-it.js
@@ -1,25 +1,11 @@
 #! /usr/bin/env node
 
-import commander from 'commander';
+// import commander from 'commander';
 import fs from 'fs';
+import inquirer from 'inquirer';
 import semver from 'semver';
 
-import { version, description } from '../package.json';
 import command from './lib/command';
-
-commander
-  .description(description)
-  .version(version);
-
-commander
-  .option(
-    '-i --increment [increment]',
-    'Version to increment [major|minor|patch] (default: patch)',
-    /^(major|minor|patch)$/i,
-    'patch',
-  );
-
-commander.parse(process.argv);
 
 // list all tag: git tag -l --sort=-v:refname
 const lastTagCommand = 'git describe --abbrev=0 --tags';
@@ -29,26 +15,39 @@ const lastTag = lastTagCommandResult.split('\n')[0];
 // Get last tag and last semver
 const lastTagSemver = lastTag.substring(1);
 
-// bump version
-const nextTagSemver = semver.inc(lastTagSemver, commander.increment);
+inquirer.prompt([
+  {
+    type: 'list',
+    name: 'increment',
+    message: 'Which version number do you want to bump?',
+    choices: ['patch', 'minor', 'major'].map(item => (
+      {
+        name: `${item} v${semver.inc(lastTagSemver, item)}`,
+        value: item,
+      }
+    )),
+  },
+]).then((answers) => {
+  const nextTagSemver = semver.inc(lastTagSemver, answers.increment);
 
-// create CHANGELOG.md if no exists
-// create changelogs
-// write changelogs into CHANGELOG
+  // create CHANGELOG.md if no exists
+  // create changelogs
+  // write changelogs into CHANGELOG
 
-// write version into package.json if exists
-if (fs.existsSync('./package.json')) {
-  const bumpNpmVersionCommand = `npm --no-git-tag-version version ${nextTagSemver}`;
-  command(bumpNpmVersionCommand);
-}
+  // write version into package.json if exists
+  if (fs.existsSync('./package.json')) {
+    const bumpNpmVersionCommand = `npm --no-git-tag-version version ${nextTagSemver}`;
+    command(bumpNpmVersionCommand);
+  }
 
-// git commit
-command(`git commit -am "Bump version to v${nextTagSemver}"`);
-const currentGitBranch = command('git rev-parse --abbrev-ref HEAD');
-command(`git push origin ${currentGitBranch}`);
+  // git commit
+  command(`git commit -am "Bump version to v${nextTagSemver}"`);
+  const currentGitBranch = command('git rev-parse --abbrev-ref HEAD');
+  command(`git push origin ${currentGitBranch}`);
 
-// git tag
-command(`git tag v${nextTagSemver}`);
+  // git tag
+  command(`git tag v${nextTagSemver}`);
 
-// git push --tags
-command('git push --tags');
+  // git push --tags
+  command('git push --tags');
+});
